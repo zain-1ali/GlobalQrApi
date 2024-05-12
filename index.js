d069ac62577a4974b8adc286862810d60bc42377
Muhammad-Abdullah <145569353+link2avicenna@users.noreply.github.com>
2024-05-12 11:41:32 -0700

    update cors

--- index.js
+++ index.js
@@ -10,18 +10,12 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 const allowedOrigins = ["http://localhost:5173"]; // Add other domains as needed
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // Check if the request origin is in the allowedOrigins array
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true); // Allow the request
-      } else {
-        callback(new Error("Not allowed by CORS")); // Block the request
-      }
-    },
-  })
-);
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use("/public/images", express.static("public/images"));
 
 app.use(express.json());
