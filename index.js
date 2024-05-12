b35990d2b8e6765049d60d96cfa12df149f64ca0
Muhammad-Abdullah <145569353+link2avicenna@users.noreply.github.com>
2024-05-12 11:45:54 -0700

    cors wild

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 const allowedOrigins = ["http://localhost:5173"]; // Add other domains as needed
-const corsOptions = {
-  origin: "http://localhost:5173",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: "http://localhost:5173",
+//   credentials: true,
+//   optionSuccessStatus: 200,
+// };
+app.use(cors({ origin: "*" }));
 app.use("/public/images", express.static("public/images"));
 
 app.use(express.json());
