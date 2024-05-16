be1b2d6a57c80baba75b0270fdcc256f57afd65d
Muhammad-Abdullah <145569353+link2avicenna@users.noreply.github.com>
2024-05-16 02:37:28 -0700

    create delete qr api

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,20 +2,20 @@ import Jwt from "jsonwebtoken";
 // import ErrorHandler from "../utils/errorHandle.js";
 
 const userAuth = async (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  // console.log(authHeader);
-  if (!authHeader) {
-    // || !authHeader.startsWith("Bearer")
-    // next("Auth Failed 1");
-    res.status(401).send({
-      status: false,
-      msg: "Auth Failed",
-    });
-    // next(new ErrorHandler("Auth Failed", 401));
-  }
-  const token = authHeader.split(" ")[1];
-
   try {
+    const authHeader = req.headers.authorization;
+    // console.log(authHeader);
+    if (!authHeader) {
+      // || !authHeader.startsWith("Bearer")
+      // next("Auth Failed 1");
+      res.status(401).send({
+        status: false,
+        msg: "Auth Failed",
+      });
+      // next(new ErrorHandler("Auth Failed", 401));
+    }
+    const token = authHeader.split(" ")[1];
+
     const payload = await Jwt.verify(token, process.env.secretkey);
     // console.log(payload.userId);
     req.userId = await payload.userId;
