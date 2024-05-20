aaa080d621c51e0130bfc30ebb432e3de949f27d
Muhammad-Abdullah <145569353+link2avicenna@users.noreply.github.com>
2024-05-20 05:03:43 -0700

    test

--- controllers/analyticsController.js
+++ controllers/analyticsController.js
@@ -206,7 +206,9 @@ export let getScansAnalytics = async (req, res, next) => {
           return dayData ? dayData.count : 0; // If data exists for the day, use count, otherwise 0
         });
         // console.log(daysData);
-        res.status(200).send({ status: true, data: daysData, qrId });
+        res
+          .status(200)
+          .send({ status: true, data: daysData, qrId, result: result });
       } else if (type === "monthly") {
         console.log("monthly");
         const endDate = new Date(); // Current date
